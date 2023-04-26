89cd105a35253a0727198c800e4e80c7bd5ebe36
Morteza Navidi <40989337+Mortezanavidi@users.noreply.github.com>
2023-04-26 07:10:24 +0200

    Update config

--- site/next.config.js
+++ site/next.config.js
@@ -4,9 +4,9 @@ const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    icon: ' https://mortezanavidi.github.io/kasm-registry/1.0/icon.png',
+    listUrl: ' https://mortezanavidi.github.io/kasm-registry/',
+    contactUrl: ' https://mortezanavidi.github.io/kasm-registry/issues/',
   },
   reactStrictMode: true,
   swcMinify: true,
